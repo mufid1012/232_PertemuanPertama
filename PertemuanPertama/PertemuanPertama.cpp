9038af3b47f16660c5e50c3aee3c83a113c725f6
mufid1012 <mufidghibran1012@gmail.com>
2024-02-27 15:28:03 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Begin
+    //numeric nAlas, nTinggi, nLuas
+    //Display 'Masukan Alas = '
+    //Accept nAlas
+    //Display 'Masukan Tinggi = '
+    //Accept nTinggi
+    //Compute nLuas = nAlas*nTinggi/2
+    //Display 'Luasnya = ' + nLuas
+    //End
+
+    float nAlas, nTinggi, nLuas;
+    cout << "Masukan Alas =";
+    cin >> nAlas;
+    cout << "Masukan Tinggi = ";
+    cin >> nTinggi;
+    nLuas = nAlas * nTinggi / 2;
+    cout << "Luasnya = " << nLuas;
+
+}
+
